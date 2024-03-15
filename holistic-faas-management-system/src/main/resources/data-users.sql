d21e89020a43bfb40ee734c2a48e5c3cd62a2277
StefanA <stefanachmueller@hotmail.com>
2024-03-15 10:42:36 +0100

    fix sql inserts

--- holistic-faas-management-system/src/main/resources/data-users.sql
+++ holistic-faas-management-system/src/main/resources/data-users.sql
@@ -1,5 +1,5 @@
-INSERT INTO hf.public.users (username, credentials_file_Path, provider) VALUES ('Stefan AWS', 'src\main\resources\auth\stefan01\aws.properties', 'AWS');
-INSERT INTO hf.public.users (username, credentials_file_Path, provider) VALUES ('Stefan GCP', 'src\main\resources\auth\stefan01\gcp.json', 'GCP');
+INSERT INTO hf.public.users (username, credentials_file_Path, provider) VALUES ('Stefan AWS', 'holistic-faas-management-system\src\main\resources\auth\stefan01\aws.properties', 'AWS');
+INSERT INTO hf.public.users (username, credentials_file_Path, provider) VALUES ('Stefan GCP', 'holistic-faas-management-system\src\main\resources\auth\stefan01\gcp.json', 'GCP');
 -- INSERT INTO hf.public.users (username) VALUES ('user2');
 -- INSERT INTO hf.public.users (username) VALUES ('user3');
 -- INSERT INTO hf.public.users (username) VALUES ('user4');
